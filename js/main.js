6379f501729999990466acadbc1ec3b94a78d23a
Timzyofficial <timzyofficial@gmail.com>
2023-01-26 15:03:24 +0300

    Pages updates

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+  $("#design").click(function()
+  {
+    $("#designtxt").toggle()
+    $("#designimg").toggle()
+  })
+
+  $("#development").click(function(){
+    $("#developtxt").toggle()
+    $("#developimg").toggle()
+  })
+
+  $("#product").click(function(){
+    $("#producttxt").toggle()
+    $("#productimg").toggle()
+  })
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  });
